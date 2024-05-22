a429adc81d64720de7fcfec2c11d32bad09a7bae
asamorokovskii <asamorokovskii@jmart.kz>
2024-05-22 18:43:30 +0500

    Homework 1 v2

--- lesson1/main.go
+++ lesson1/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/samorokowsky/lessongo/greet"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	name := os.Args[1]
-	tableStr := os.Args[2]
+	reader := bufio.NewReader(os.Stdin)
+
+	name := getCLIValue(reader, "name")
+	tableStr := getCLIValue(reader, "table")
 
 	table, err := strconv.Atoi(tableStr)
 
@@ -20,4 +24,13 @@ func main() {
 
 	text := greet.Greet(name, table)
 	fmt.Println(text)
+}
+
+func getCLIValue(reader *bufio.Reader, propertyName string) string {
+	fmt.Printf("Enter %s: ", propertyName)
+
+	readValue, _ := reader.ReadString('\n')
+	readValue = strings.Trim(readValue, "\r\n")
+
+	return readValue
 }
